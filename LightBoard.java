6efd33e98e360e8c718cd562a826607396796f61
AdamFekkah <112909739+AdamFekkah@users.noreply.github.com>
2025-05-05 05:35:59 +0000

    all

--- LightBoard.java
+++ LightBoard.java
@@ -1,19 +1,58 @@
-public class LightBoard
-{
- /** The lights on the board, where true represents on and false represents off.
- */
- private boolean[][] lights;
- /** Constructs a LightBoard object having numRows rows and numCols columns.
- * Precondition: numRows > 0, numCols > 0
- * Postcondition: each light has a 40% probability of being set to on.
- */
- public LightBoard(int numRows, int numCols)
- { /* to be implemented in part (a) */ }
- /** Evaluates a light in row index row and column index col and returns a status
- * as described in part (b).
- * Precondition: row and col are valid indexes in lights.
- */
- public boolean evaluateLight(int row, int col)
- { /* to be implemented in part (b) */ }
- // There may be additional instance variables, constructors, and methods not shown.
-} 
+public class LightBoard {
+    private boolean[][] lights;
+
+    /** Constructs a LightBoard object with numRows and numCols lights.
+     *  Each light is turned on with 40% probability.
+     */
+    public LightBoard(int numRows, int numCols) {
+        lights = new boolean[numRows][numCols];
+
+        for (int r = 0; r < lights.length; r++) {
+            for (int c = 0; c < lights[0].length; c++) {
+                if (Math.random() < 0.4) {
+                    lights[r][c] = true;
+                }
+            }
+        }
+    }
+
+    /** For testing with specific configurations */
+    public void setLights(boolean[][] arr) {
+        lights = arr;
+    }
+
+    /** Returns whether the light should be on or off based on special rules */
+    public boolean evaluateLight(int row, int col) {
+        boolean light = lights[row][col];
+        int colLights = 0;
+
+        for (int r = 0; r < lights.length; r++) {
+            if (lights[r][col]) {
+                colLights++;
+            }
+        }
+
+        if (light && colLights % 2 == 0) {
+            return false;
+        }
+
+        if (!light && colLights % 3 == 0) {
+            return true;
+        }
+
+        return light;
+    }
+
+    /** Returns the proportion of lights that are on */
+    public double test() {
+        double on = 0;
+
+        for (boolean[] row : lights) {
+            for (boolean b : row) {
+                if (b) on++;
+            }
+        }
+
+        return on / (lights.length * lights[0].length);
+    }
+}
--- Main.java
+++ Main.java
@@ -1,6 +1,25 @@
-public class Main
-{
-  public static void main(String[] args)
-  {
-  }
+public class Main {
+    public static void main(String[] args) {
+        LightBoard sim = new LightBoard(7, 5);
+
+        boolean[][] lights = {
+            {true,  true,  false, true,  true },
+            {true,  false, false, true,  false},
+            {true,  false, false, false, true },
+            {true,  false, false, false, true },
+            {true,  false, false, false, true },
+            {true,  true,  false, true,  true },
+            {false, false, false, false, false}
+        };
+
+        sim.setLights(lights);
+
+        System.out.println(sim.evaluateLight(0, 3));
+        System.out.println(sim.evaluateLight(6, 0));
+        System.out.println(sim.evaluateLight(4, 1));
+        System.out.println(sim.evaluateLight(5, 4));
+
+        // Optional: test light density
+        System.out.println("Proportion on: " + sim.test());
+    }
 }
